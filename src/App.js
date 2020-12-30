a790feb991e8edc06cabb0d942ecb79a8a00c5d2
himsch <55rsearchco@gmail.com>
2020-12-30 16:15:50 +0900

    Component Key value add

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
 import React from "react";
 
-function Food({ name, picture }) {
-  return (
-    <div>
-      <h2>I Like {name}</h2>
-      <img src={picture} alt="음식사진" />
-    </div>
-  );
-}
-
 const foodILike = [
   {
+    id: 1,
     name: "Kimchi",
     img:
       "https://www.bgw.kr/wp-content/uploads/2019/12/%EC%88%98%EC%9E%85%EA%B9%80%EC%B9%98.png",
   },
   {
+    id: 2,
     name: "Doncasu",
     img:
       "http://th3.tmon.kr/thumbs/image/6ea/082/d57/2906e06a0_700x700_95_FIT.jpg",
   },
   {
+    id: 3,
     name: "Kimbap",
     img: "https://craftlog.com/m/i/5805364=s1280=h960.webp",
   },
 ];
 
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>I Like {name}</h2>
+      <img src={picture} alt={name} />
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
       {foodILike.map(dish => (
-        <Food name={dish.name} picture={dish.img} />
+        <Food key={dish.id} name={dish.name} picture={dish.img} />
       ))}
     </div>
   );
